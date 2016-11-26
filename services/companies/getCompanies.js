9e4d67d8272690d975867c386f9813865cb82648
Eduardo Romo <EduRomoGO@MacBook-Pro-de-Eduardo.local>
2016-11-26 18:45:28 +0100

    first commit

---
+++ services/companies/getCompanies.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function getCompaniesService (req, res) {
+	res.send({a: 3});
+}
---
+++ static/js/companies.js
@@ -0,0 +1,32 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	var elem = document.getElementById('companies-list');
+
+	var companyList = JSON.parse(document.getElementById("companyList").childNodes[0].data);
+
+	function removeElem (event) {
+		document.getElementById('companies-list').removeChild(this);
+	}
+
+	function loadCompanies () {
+		companyList.forEach(function (company, index) {
+			var content = document.querySelector('#company-item-template').content;
+			
+			content.querySelector('.name').textContent = company.company;
+			content.querySelector('.description').textContent = company.description;
+			content.querySelector('.email').textContent = company.email;
+			content.querySelector('.url').textContent = company.url;
+			content.querySelector('.country').textContent = company.country;
+
+			document.querySelector('#companies-list').appendChild(
+				document.importNode(content, true)
+			);
+			
+			document.getElementsByClassName('company')[index].addEventListener('click', removeElem);
+		});
+	}
+
+	loadCompanies();
+});
