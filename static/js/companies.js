6cf2f7d443e8ba74db03efe82f34a98e7b7306a9
Eduardo Romo <EduRomoGO@MacBook-Pro-de-Eduardo.local>
2016-11-26 19:45:26 +0100

    now the companies list is retrieved from a service instead of having it in the index.html document

--- static/js/companies.js
+++ static/js/companies.js
@@ -2,31 +2,30 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	$.getJSON('/companies', function (data) {
-		console.log(data);
-	});
-
-	var companyList = JSON.parse(document.getElementById("companyList").childNodes[0].data);
-
-	function removeElem (event) {
-		document.getElementById('companies-list').removeChild(this);
-	}
-
 	function loadCompanies () {
-		companyList.forEach(function (company, index) {
-			var content = document.querySelector('#company-item-template').content;
-			
-			content.querySelector('.name').textContent = company.company;
-			content.querySelector('.description').textContent = company.description;
-			content.querySelector('.email').textContent = company.email;
-			content.querySelector('.url').textContent = company.url;
-			content.querySelector('.country').textContent = company.country;
 
-			document.querySelector('#companies-list').appendChild(
-				document.importNode(content, true)
-			);
-			
-			document.getElementsByClassName('company')[index].addEventListener('click', removeElem);
+		function removeElem (event) {
+			document.getElementById('companies-list').removeChild(this);
+		}
+
+		$.getJSON('/companies', function (companyList) {
+
+			companyList.forEach(function (company, index) {
+				var content = document.querySelector('#company-item-template').content;
+				
+				content.querySelector('.name').textContent = company.company;
+				content.querySelector('.description').textContent = company.description;
+				content.querySelector('.email').textContent = company.email;
+				content.querySelector('.url').textContent = company.url;
+				content.querySelector('.country').textContent = company.country;
+
+				document.querySelector('#companies-list').appendChild(
+					document.importNode(content, true)
+				);
+				
+				document.getElementsByClassName('company')[index].addEventListener('click', removeElem);
+			});
+
 		});
 	}
 
