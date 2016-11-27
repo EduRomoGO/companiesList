fc63cca0d4e113c804dc55ab374ad1636f45d305
Eduardo Romo <EduRomoGO@MacBook-Pro-de-Eduardo.local>
2016-11-27 20:26:37 +0100

    refactorization to extract modal functionality to an external file

--- static/js/src/loadCompanies.js
+++ static/js/src/loadCompanies.js
@@ -1,59 +1,6 @@
 'use strict';
 
-function createRemoveConfirmationDomElement ({company}) {
-	var	removeConfirmationTemplate = document.querySelector('#remove-confirmation-template').content,
-		msg = 'Do you want to remove <b>' + company.attributes.data.value + '</b> company?';
-
-	removeConfirmationTemplate.querySelector('.confirmation').innerHTML = msg;
-
-	document.querySelector('#remove-confirmation-dest').appendChild(
-		document.importNode(removeConfirmationTemplate, true)
-	);
-}
-
-function removeConfirmationModal (modal) {
-	var modal = modal || document.getElementsByClassName('remove-confirmation')[0];
-
-    document.getElementById('remove-confirmation-dest').removeChild(modal);
-}
-
-
-function yesClick ({company}) {
-	removeConfirmationModal();
-    removeCompanyItem({company});	
-}
-
-function addListenersForConfirmationModal ({company}) {
-	window.addEventListener('click', function(event) {
-		var modal = document.getElementsByClassName('remove-confirmation')[0];
-
-	    if (event.target == modal) {
-	        removeConfirmationModal();
-	    }
-	});
-
-	document.getElementsByClassName("close")[0].addEventListener('click', function () {
-		removeConfirmationModal();
-	});
-
-	document.querySelector('.remove-confirmation-button').addEventListener('click', function () {
-		yesClick({company});
-	});
-}
-
-function displayConfirmation () {
-	var company = this;
-	console.log(this);
-
-	createRemoveConfirmationDomElement({company});
-	addListenersForConfirmationModal({company});
-}
-
-function removeCompanyItem ({company}) {
-	var company = company || this;
-
-	document.getElementById('companies-list').removeChild(company);
-}
+const removeCompanyModal = require('./removeCompanyModal.js');
 
 function fillTemplate ({companyTemplate, company}) {
 	companyTemplate.querySelector('.name').textContent = company.company;
@@ -75,7 +22,7 @@ function createCompanyDomElement (company, index) {
 	fillTemplate({companyTemplate, company});
 	appendAndStampTemplate({companyTemplate});
 	document.getElementsByClassName('company')[index].setAttribute('data', company.company);
-	document.getElementsByClassName('company')[index].addEventListener('click', displayConfirmation);
+	document.getElementsByClassName('company')[index].addEventListener('click', removeCompanyModal.displayConfirmation);
 }
 
 function createCompanyListDomElements (companyList) {
